a0b40ecbff06d0903c688cb1ac1b290d543605c2
Simon Legner <Simon.Legner@gmail.com>
2014-01-27 21:24:56 +0100

    Use kazuhikoarase's JavaScript QRCode generator

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 qrcode-bookmarklet
 =
 
-This bookmarklet is based on Shaun's qrbookmarklet from https://code.google.com/p/qrbookmarklet/
+This bookmarklet is based on shaun's qrbookmarklet from https://code.google.com/p/qrbookmarklet/ and uses kazuhikoarase's JavaScript QRCode generator https://github.com/kazuhikoarase/qrcode-generator.
 
 To install, create a bookmark of
 ```
-javascript:(function(){if(document.getElementById){var x=document.body;var o=document.createElement('script');if(typeof(o)!='object') o=document.standardCreateElement('script');o.setAttribute('src','https://qrbookmarklet.googlecode.com/svn/trunk/qr.js');o.setAttribute('type','text/javascript');x.appendChild(o);}})(); 
+javascript:(function(){document.body.appendChild(document.createElement('script')).src='http://localhost:8000/qrcode.js';document.body.appendChild(document.createElement('script')).src='http://localhost:8000/qr.js';})();
 ```
 
 This project is MIT licensed.
--- qr.js
+++ qr.js
@@ -18,49 +18,37 @@ function getSelected() {
 }
 
 function cleanUp() {
-	node = document.getElementsByTagName('body')[0];
-	var a = [];
-	var re = new RegExp('\\bshaunsational_qr\\b');
-	var els = node.getElementsByTagName('*');
-	for (var i=0, j=els.length; i < j; i++) {
-		if (re.test(els[i].className)) {
-			a.push(els[i]);
-		}
-	}
-
-	for (var x in a) {
-		if (a[x].parentNode) {
-			a[x].parentNode.removeChild(a[x]);
-		}
+	var node = document.getElementsByTagName('body')[0];
+	var els = node.getElementsByClassName('shaunsational_qr');
+	for (var i = els.length; i > 0; i--) {
+		els[0].parentNode.removeChild(els[0]);
 	}
 }
 cleanUp();
 
 var selectedText = getSelected();
-var qrData = (selectedText === false) ? customURL(document.location.href) : selectedText;
+var qrData = (selectedText == false) ? customURL(document.location.href) : selectedText;
 
 var shade = document.createElement('div');
 	shade.setAttribute('class','shaunsational_qr');
 	shade.setAttribute('id','shade');
 	shade.setAttribute('style','background-color:#000;height:100%;left:0;opacity:0.65;position:fixed;top:0;width:100%;z-index:1001;');
-	
+
 var centr = document.createElement('div');
 	centr.setAttribute('class','shaunsational_qr');
 	centr.setAttribute('id','centr');
 	centr.setAttribute('style','background-color:transparent;height:100%;left:0;position:fixed;text-align:center;top:0;width:100%;z-index:1001;');
-	
+
+var qr = qrcode(4, 'H');
+	qr.addData(qrData);
+	qr.make();
+
 var qrdiv = document.createElement('div');
 	qrdiv.setAttribute('class','shaunsational_qr');
 	qrdiv.setAttribute('id','qrdiv');
 	qrdiv.setAttribute('style','margin:25px auto auto auto;background-color:#FFF;height:320px;width:320px;z-index:1001;-moz-border-radius:20px;');
-   
-var qrcode = document.createElement('img');
-	qrcode.setAttribute('class','shaunsational_qr');
-	qrcode.setAttribute('id','qrcode');
-	qrcode.setAttribute('style','margin:10px;cursor:pointer;');
-	qrcode.setAttribute('src','https://chart.googleapis.com/chart?cht=qr&chs=300x300&chld=H|0&chl=' + qrData);
-	qrcode.setAttribute('title','Click to close.');
-	
+	qrdiv.innerHTML = qr.createImgTag(8);
+
 var qrurl = document.createElement('div');
 	qrurl.setAttribute('class','shaunsational_qr');
 	qrurl.setAttribute('id','qrurl');
@@ -71,7 +59,6 @@ var qrurl_text = document.createTextNode(qrData);
 
 centr.appendChild(qrdiv);
 centr.appendChild(qrurl);
-qrdiv.appendChild(qrcode);
 document.body.appendChild(shade);
 document.body.appendChild(centr);
 document.body.setAttribute('onclick','cleanUp()');
